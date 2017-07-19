df13b623c765209727f24e091bf7113771a1fad3
AkiraHakuta <ahkt9898@gmail.com>
2017-07-19 18:03:52 +0900

    Add files via upload

--- tex2sym_lexer.py
+++ tex2sym_lexer.py
@@ -1,4 +1,4 @@
-# tex2sym_lexer.py   Author: Akira Hakuta, Date: 2017/07/13
+# tex2sym_lexer.py   Author: Akira Hakuta, Date: 2017/07/19
 # python.exe tex2sym_lexer.py
 
 from ply import lex
@@ -8,11 +8,12 @@ tokens = ('EXPONENT', 'FACTORIAL', 'MULT', 'DIV', 'PLUS', 'MINUS',
         'LBRACE', 'RBRACE', 'LPAREN', 'RPAREN','UB', 'LBRACKET','RBRACKET',
         'LPIPE', 'RPIPE',
         'PI', 'IMAGINARY_UNIT', 'NAPIER_CONSTANT', 
-        'NN_FLOAT', 'GREEK_CH', 'DX','NN_INTEGER','ALPHABET',
+        'NN_FLOAT', 'GREEK_CH', 'DGREEK_CH', 'DX','NN_INTEGER','ALPHABET',
         'LIM', 'INFTY', 'TO', 'DIFF',       
-        'F_FRAC', 'F_SQRT',  'F_TRIG','F_TRIG_CAR', 'F_LOG','F_LOG_UB','F_SUM', 'F_INT', 'F_SEQ_TERM',
+        'F_FRAC', 'F_SQRT',  'F_TRIG','F_TRIG_CAR', 'F_LOG','F_LOG_UB','F_SUM', 'F_INT', 'F_SEQ_TERM', 'F_GAMMA', 'F_ZETA',
         'COMBI_PERMU',
-        'EQUAL', 'RELATION',)
+        'EQUAL', 'RELATION',
+        'FUNCTION',)
 
 # Define `t_ignore` to ignore unnecessary characters between tokens, such as whitespaces.
 t_ignore = " \t"
@@ -22,25 +23,31 @@ t_ignore = " \t"
 # The name of functions must be like `t_<token_name>`.
 # All tokens defined by functions are added in the same order as they appear in the lexer file.
 
+
 def t_EXPONENT(t):
     r'\^'
     return t
+ 
  
 def t_FACTORIAL(t):
     r'!'
     return t    
 
+
 def t_MULT(t):
     r'\*|\\times|\\cdot'
     return t
 
+
 def t_DIV(t):
     r'\\div'
     return t  
+ 
     
 def t_PLUS(t):
     r'\+' 
     return t
+
 
 def t_MINUS(t):
     r'\-'
@@ -51,18 +58,21 @@ def t_LBRACE(t):
     r'\{'
     return t
 
+
 def t_RBRACE(t):
     r'\}'
     return t
    
     
 def t_LPAREN(t):
-    r'\('
+    r'\(|\\left\('
     return t
 
+
 def t_RPAREN(t):
-    r'\)'
+    r'\)|\\right\)'
     return t
+
 
 def t_LBRACKET(t):
     r'\['
@@ -71,6 +81,7 @@ def t_LBRACKET(t):
 def t_RBRACKET(t):
     r'\]'
     return t
+
 
 def t_LPIPE(t):
     r'\\left\|'
@@ -84,84 +95,122 @@ def t_RPIPE(t):
 def t_PI(t):
     r'\\ppi'
     return t
+ 
     
 def t_IMAGINARY_UNIT(t):
     r'\\ii'
     return t
+ 
     
 def t_NAPIER_CONSTANT(t):
     r'\\ee'
     return t  
+
     
 def t_GREEK_CH(t):
     r'aalpha|bbeta|ggamma|ttheta|oomega'
     return t
 
-def t_DX(t):
-    r'daalpha|dbbeta|dggamma|dttheta|doomega|d[a-z]'
+
+def t_DGREEK_CH(t):
+    r'daalpha|dbbeta|dggamma|dttheta|doomega'
     return t
+  
+    
+def t_DX(t):
+    r'd[a-z]'
+    return t
+  
     
 def t_NN_FLOAT(t):
     r'\d*\.\d+'
     return t
+  
     
 def t_NN_INTEGER(t):
     r'\d+'
     return t  
+ 
       
 def t_F_SEQ_TERM(t):
     r'[a-z]_'
     return t
+
+def t_FUNCTION(t):
+    r'f\(|f\\left\(|g\(|g\\left\('
+    return t
+  
     
 def t_ALPHABET(t):
-    r'[a-zABCXYZ]'
+    r'[a-zA-DF-Z]'
     return t
     
 
    
 def t_DIFF(t):
-    r'\\frac\{d\}|\\dfrac\{d\}'
+    r'\\frac\{d|\\dfrac\{d'
     return t    
+ 
  
 def t_F_FRAC(t):
     r'\\frac|\\dfrac'
     return t
+ 
              
 def t_F_SQRT(t):
     r'\\sqrt'
     return t
+ 
         
 def t_F_TRIG_CAR(t):
     r'\\sin\^|\\cos\^|\\tan\^'
     return t
+ 
     
 def t_F_TRIG(t):
     r'\\sin|\\cos|\\tan'
     return t
+ 
     
 def t_F_LOG_UB(t):
     r'\\log_'
     return t
+ 
     
 def t_F_LOG(t):
     r'\\log'
     return t
+ 
+    
+def t_F_GAMMA(t):
+    r'\\Gamma\(|\\Gamma\\left\('
+    return t
+ 
+    
+def t_F_ZETA(t):
+    r'\\zeta\(|\\zeta\\left\('
+    return t
+   
     
 def t_UB(t):
     r'_'
     return t   
+  
     
 def t_F_SUM(t):
     r'\\sum'
     return t
 
+
 def t_LIM(t):
     r'\\lim'
     return t
 
+
 def t_INFTY(t):
     r'\\infty'
     return t
+  
     
 def t_TO(t):
     r'\\to'
@@ -171,6 +220,7 @@ def t_TO(t):
 def t_F_INT(t):
     r'\\int'
     return t
+ 
     
 def t_COMBI_PERMU(t):
     r'\\C|\\P'
@@ -244,5 +294,8 @@ if __name__ == '__main__':
     print(test_lexer('10!'))
     print(test_lexer('_{5}\\C_{2}'))
     print(test_lexer('_{5}\\P_{2}'))    
+    print(test_lexer('f(x)g(x)'))
+    print(test_lexer('\\Gamma(x)'))
+    print(test_lexer('\\zeta(x)'))
      
     
--- tex2sym_parser.py
+++ tex2sym_parser.py
@@ -1,14 +1,13 @@
-# tex2sym_parser.py   Author: Akira Hakuta, Date: 2017/07/13
+# tex2sym_parser.py   Author: Akira Hakuta, Date: 2017/07/19
 # python.exe tex2sym_parser.py
 
 from ply import yacc
 # Get the token map
 from tex2sym_lexer import tokens, lexer
 
-from sympy import *
-var('a:z') 
+from sympy import * 
 
-# variable : a,b,...,z,A,B,C,X,Y,Z,\\alpha,\\beta,\\gamma,\\theta,\\omega
+# variable : a,b,...,z,A,B,...,D,  F,G,...,Y,Z,\\alpha,\\beta,\\gamma,\\theta,\\omega
 # constant : pi --> \\ppi, imaginary unit --> \\ii, napier constant --> \\ee
 
 precedence = (
@@ -22,16 +21,19 @@ precedence = (
 # Parsing rules
 # Functions should be start with `p_`.
 
+
 # statement : expr
 def p_statement(p):
     'statement : expr'
     p[0] = p[1]
+
 
 # expr : expr^expr
 def p_expr_exponent(p):
     'expr : expr EXPONENT expr'
     #p[0]  p[1] p[2]   p[3]
     p[0] = '({})**({})'.format(p[1], p[3])
+ 
     
 # expr : expr!
 def p_expr_factorial(p):
@@ -43,11 +45,13 @@ def p_expr_factorial(p):
 def p_expr_mult(p):
     'expr : expr MULT expr'
     p[0] = '{}*{}'.format(p[1],p[3])
+  
     
 # expr : expr expr    
 def p_expr_exprexpr(p):
     'expr : expr expr'
     p[0] = '{}*{}'.format(p[1],p[2])
+ 
               
 # expr : expr \div expr
 def p_expr_div(p):
@@ -65,16 +69,19 @@ def p_expr_plus(p):
 def p_expr_minus(p):
     'expr : expr MINUS expr'
     p[0] = '{}-{}'.format(p[1], p[3])
+
     
 # expr : {expr}
 def p_expr_brace(p):
     'expr : LBRACE expr RBRACE'
     p[0] = '({})'.format(p[2])
 
+
 # expr :  (expr)
 def p_expr_paren(p):
     'expr : LPAREN expr RPAREN'
     p[0] = '({})'.format(p[2])
+
 
 # expr : [a-zABCXYZ]
 def p_expr_symbol(p):
@@ -86,51 +93,61 @@ def p_expr_greek_ch(p):
     'expr : GREEK_CH'
     p[0] = p[1]
 
+
 # expr : +expr
 def p_expr_plus_expr(p):
     'expr : PLUS expr %prec UPLUS' # override precedence of PLUS by `%prec UPLUS`
     p[0] = p[2]
 
+
 # expr : -expr
 def p_expr_minus_expr(p):
     'expr : MINUS expr %prec UMINUS' # override precedence of MINUS by `%prec UMINUS`
     p[0] = '(-1)*({})'.format(p[2])
+ 
     
 # expr : pi
 def p_expr_pi(p):
     'expr : PI'
     p[0] = S.Pi
  
+ 
 # expr : imaginary_unit    
 def p_expr_imaginary_unit(p):
     'expr : IMAGINARY_UNIT'
     p[0] = S.ImaginaryUnit
+
 
 # expr : napier_constant
 def p_expr_napier_constant(p):
     'expr : NAPIER_CONSTANT'
     p[0] = S.Exp1    
 
+
 # expr : infty
 def p_expr_infty(p):
     'expr : INFTY'
     p[0] = 'oo'
+
 
 # expr :  \d+  
 def p_expr_integer(p):
     'expr : NN_INTEGER'
     p[0] = p[1]
  
+ 
 # expr :  \d*\.\d+ 
 def p_expr_float(p):
     'expr : NN_FLOAT'
     #p[0] = 'nsimplify(Rational({}))'.format(p[1])
     p[0] = 'nsimplify({})'.format(p[1])
+ 
         
 # expr : \\sqrt{expr}
 def p_expr_sqrt1(p):
     'expr : F_SQRT LBRACE expr RBRACE'
     p[0] = 'sqrt({})'.format(p[3])    
+
     
 # expr : \\sqrt[expr]{expr}
 def p_expr_sqrt2(p):
@@ -138,62 +155,89 @@ def p_expr_sqrt2(p):
     #p[0] = '(root(({}),({})))'.format(p[6],p[3])
     p[0] = '(({})**(({})**(-1)))'.format(p[6],p[3])
 
+
 # expr : \\frac{expr}{expr}   expr : \\dfrac{expr}{expr}
 def p_expr_frac(p):
     'expr : F_FRAC LBRACE expr RBRACE LBRACE expr RBRACE'
     p[0] = '({}) * ({})**(-1)'.format(p[3], p[6])    
 
+
 # expr : \\sin{expr} | \\cos{expr} | \\tan{expr} 
 def p_expr_f_trigonometric(p):
     'expr : F_TRIG LBRACE expr RBRACE'
     p[0] = '{}({})'.format(p[1][1:], p[3])
+  
      
 # expr : \\log{expr}
 def p_expr_f_log(p):
     'expr : F_LOG LBRACE expr RBRACE'
     p[0] = 'log({})'.format(p[3])
 
+
 # expr : \\sin^{expr}{expr} | \\cos^{expr}{expr} | \\tan^{expr}{expr} 
 def p_expr_f_trigonometric_car(p):
     'expr : F_TRIG_CAR LBRACE expr RBRACE LBRACE expr RBRACE'
     p[0] = '({}({}))**({})'.format(p[1][1:4],p[6],p[3])  
 
+
 # expr : \\log_{expr}{expr}
 def p_expr_f_log_ub(p):
     'expr : F_LOG_UB LBRACE expr RBRACE LBRACE expr RBRACE '
     p[0] = 'log({})*(log({})**(-1))'.format(p[6],p[3])
+ 
+    
+# expr : \\Gamma(expr)
+def p_expr_f_gamma(p):
+    'expr : F_GAMMA expr RPAREN'
+    p[0] = 'gamma({})'.format(p[2])
+ 
+    
+# expr : \\zeta(expr)
+def p_expr_f_zeta(p):
+    'expr : F_ZETA expr RPAREN'
+    p[0] = 'zeta({})'.format(p[2])
+ 
     
 # expr : \\sum_{k=expr}^{expr}{expr}
 def p_expr_sum(p):
     'expr : F_SUM  UB LBRACE ALPHABET EQUAL expr  RBRACE EXPONENT LBRACE expr RBRACE LBRACE expr RBRACE'
     p[0] = 'summation({},({},{},{}))'.format(p[13],p[4],p[6],p[10])
+
  
-# expr : \\frac{d}{dx} {expr}
+# expr : \\frac{d}{dx}{expr}
 def p_expr_diff(p):
-    'expr : DIFF LBRACE DX RBRACE LBRACE expr RBRACE'
-    x=p[3][1:]    
-    if x=='aalpha' or x=='bbeta' or x=='ggamma' or x=='ttheta' or x=='oomega':
-        p[0] = 'diff({},{})'.format(p[6], p[3][1:])
-    else:
-        p[0] = 'diff({},{})'.format(p[6], p[3][1]) 
+    '''expr : DIFF  RBRACE LBRACE DGREEK_CH RBRACE LBRACE expr RBRACE
+            | DIFF  RBRACE LBRACE DX        RBRACE LBRACE expr RBRACE'''
+    p[0] = 'diff({},{})'.format(p[7], p[4][1:]) 
+ 
     
+# expr : (\\frac{d}{dx})^{n} {expr}
+def p_expr_diff_n1(p):
+    '''expr :  LPAREN DIFF RBRACE LBRACE DGREEK_CH RBRACE RPAREN EXPONENT LBRACE expr RBRACE LBRACE expr RBRACE
+            |  LPAREN DIFF RBRACE LBRACE DX        RBRACE RPAREN EXPONENT LBRACE expr RBRACE LBRACE expr RBRACE'''
+    p[0] = 'diff({},{},{})'.format(p[13], p[5][1:], p[10])
+    
+            
+# expr : \\frac{d^{n}}{dx^{n}} {expr}
+def p_expr_diff_n2(p):
+    '''expr : DIFF EXPONENT LBRACE expr RBRACE RBRACE LBRACE DGREEK_CH  EXPONENT LBRACE expr RBRACE RBRACE LBRACE expr RBRACE
+            | DIFF EXPONENT LBRACE expr RBRACE RBRACE LBRACE DX         EXPONENT LBRACE expr RBRACE RBRACE LBRACE expr RBRACE'''
+    p[0] = 'diff({},{},{})'.format(p[15], p[8][1:], p[4])
+        
+
 # expr : \\int{expr dx}
 def p_expr_int(p):
-    'expr : F_INT LBRACE expr DX RBRACE'
-    x=p[4][1:]
-    if x=='aalpha' or x=='bbeta' or x=='ggamma' or x=='ttheta' or x=='oomega':
-        p[0] = 'integrate({},{})'.format(p[3],p[4][1:])
-    else:
-        p[0] = 'integrate({},{})'.format(p[3],p[4][1])    
+    '''expr : F_INT LBRACE expr DGREEK_CH RBRACE
+            | F_INT LBRACE expr DX        RBRACE'''
+    p[0] = 'integrate({},{})'.format(p[3],p[4][1:])
+    
 
 # expr : \\int^{expr}_{expr}{expr dx}
 def p_expr_definite_int(p):
-    'expr : F_INT UB LBRACE expr RBRACE EXPONENT LBRACE expr RBRACE LBRACE expr DX RBRACE'
-    x=p[8][1:]
-    if x=='aalpha' or x=='bbeta' or x=='ggamma' or x=='ttheta' or x=='oomega':
-        p[0] = 'integrate({},({},{},{}))'.format(p[11],p[12][1:],p[4],p[8])
-    else:
-        p[0] = 'integrate({},({},{},{}))'.format(p[11],p[12][1],p[4],p[8])
+    '''expr : F_INT UB LBRACE expr RBRACE EXPONENT LBRACE expr RBRACE LBRACE expr DGREEK_CH RBRACE
+            | F_INT UB LBRACE expr RBRACE EXPONENT LBRACE expr RBRACE LBRACE expr DX        RBRACE'''
+    p[0] = 'integrate({},({},{},{}))'.format(p[11],p[12][1:],p[4],p[8])
+ 
     
 # expr : \\lim_{expr->expr}{expr}
 def p_expr_lim(p):
@@ -201,11 +245,12 @@ def p_expr_lim(p):
     p[0] = 'limit({}, {}, {})'.format(p[9],p[4],p[6])
     
 
-F=Function('F')
+#F=Function('F')
 # expr : a_{expr}
 def p_expr_seq_term(p):
     'expr : F_SEQ_TERM LBRACE expr RBRACE'
     p[0] = 'F({})'.format(p[3]) 
+ 
           
 # expr : _{expr}\\C_{expr} |  _{expr}\\P_{expr}
 def p_expr_combi_or_permutation(p):
@@ -214,12 +259,20 @@ def p_expr_combi_or_permutation(p):
         p[0] = 'binomial({},{})'.format(p[3],p[8])
     elif p[5] == '\\P':
         p[0] = 'ff({},{})'.format(p[3],p[8])
+ 
         
 # expr : \\left| expr \\right|
 def p_expr_abs(p):
     'expr : LPIPE expr RPIPE'
     p[0] = 'Abs({})'.format(p[2])        
-       
+ 
+
+
+    
+# expr : f(expr)
+def p_expr_function(p):
+    'expr : FUNCTION expr RPAREN'
+    p[0] = 'f({})'.format(p[2])         
 
 
 # statement : expr = expr
@@ -241,6 +294,8 @@ def p_statement_relation_expr(p):
         p[0] = '{}<={}'.format(p[1],p[3])   
 
 
+
+
 # Rule for error handling
 def p_error(t):
     print("syntax error at '%s'" % (t.value))
@@ -258,7 +313,7 @@ logging.basicConfig(
 
 
 def tex2sym(texexpr):
-    replace_list=[['~',''],['\,',''],['\:',''],['\;',''],['\!',''], ['\\{','('],['\\}', ')'],['\\left(','('],['\\right)', ')'],        
+    replace_list=[['~',''],['\,',''],['\:',''],['\;',''],['\!',''], ['\\{','('],['\\}', ')'],        
         ['\\alpha','aalpha'],['\\beta','bbeta'],['\\gamma','ggamma'],['\\omega','oomega'],['\\theta','ttheta']]
     for le in replace_list:
         texexpr=texexpr.replace(le[0],le[1]) 
@@ -285,28 +340,34 @@ def test(texexpr):
     print(texexpr.replace('\\','\\\\')+' --> '+ tex2sym(texexpr))
     
 if __name__ == '__main__':
+    #f=Function('f')
     test('2^3')
     test('0.5 \\times 3 \\div 5 \\cdot 4')
     test('2*a*b^2*c^3')
     test('2ab^2c^3')
-    test('2AB^2C^3')
     test('\\sqrt{3x}')
     test('\\frac{2}{3}')
-    test('\\dfrac{2}{3}')
     test('\\sin {\\ppi x}')#\sin \ppi x bad
+    test('\\log{\\ee^3}')#\log \ee^3 bad
+    test('(\\frac{d}{dx})^{3}{x^5}')
+    test('\\int{\\sin^{2}{x} dx}')
+    test('\\sum_{k=1}^{n}{k^3}')
+    test('\\Gamma(x)')
+    test('\\zeta(x)')    
+    test('2x^2+3x+4=0')
+    test('\\dfrac{d^{2}}{dx^{2}}{f(x)}=-f(x)')
+    
+    '''
+    test('\\dfrac{2}{3}')    
     test('\\sin {x} \\cos {x} \\tan {x}')
     test('\\sin {x} + \\cos {x} + \\tan {x}')
     test('\\sin^{2}{x} \\cos^{2}{x} \\tan^{2}{x}')#\sin^k x, \cos^{10} x bad 
-    test('\\sin^{3}{x} + \\cos^{3}{x} + \\tan^{3}{x}')  
-    test('\\log{\\ee^3}')#\log \ee^3 bad
+    test('\\sin^{3}{x} + \\cos^{3}{x} + \\tan^{3}{x}')     
     test('\\log_{2}{8}')#\log_2 8 bad 
-    test('\\frac{d}{dx}{x^3}')
-    test('\\int{\sin^{2}{x} dx}')
-    test('\\int_{1}^{3}{(x-1)(x-3)^2 dx}')
-    test('\\sum_{k=1}^{n}{k^3}')
+    test('\\frac{d}{dx}{x^3}')    
+    test('\\int_{1}^{3}{(x-1)(x-3)^2 dx}')    
     test('\\lim_{x \\to -\\infty}{(\\sqrt{x^2+3x}+x)}')
-    test('12a_{n+1}-35a_{n}')#35a_n bad
-    test('2x^2+3x+4=0')
+    test('12a_{n+1}-35a_{n}')#35a_n bad    
     test('x^2-3x-4 \\leqq 0')
     test('\\left| \\left| 3-\\ppi \\right|-1\\right|')#| | 3 - \ppi | -1 | bad
     test('10!')
@@ -316,6 +377,8 @@ if __name__ == '__main__':
     test('\\{a-2(b-c)\\}^2')
     test('\\left\\{a-2(b-c)\\right\\}^2')
     test('\\left\\{A-2(B-C)\\right\\}^2')
-    test(r'\int{t^2\log{t} \,dt}')
-    
+    test('\\int{t^2\\log{t} dt}')    
+    test('\\frac{d^{2}}{dx^{2}}{x^5}')        
+    test('\\frac{d}{daalpha}{aalpha^3}')
+    '''
     
